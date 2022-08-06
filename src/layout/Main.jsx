133d1c8b8ed5f4fd86e8d7d175bbc1d85ecb7120
Vyshniakov Ihor <ihor.vyshniakov@qlicks.nl>
2022-08-06 21:09:46 +0300

    46. Replaced API key to .env.local

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,7 +3,7 @@ import { Movies } from '../components/Movies';
 import { Search } from '../components/Search';
 import { Preloader } from '../components/Preloader';
 
-// TODO: Fix bug of filtering
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class Main extends React.Component {
 	state = {
@@ -14,7 +14,7 @@ class Main extends React.Component {
 	searchMovies = (search, type = 'all') => {
 		this.setState({ loading: true });
 		fetch(
-			`http://www.omdbapi.com/?apikey=4abf06ce&s=${search}${
+			`http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}${
 				type !== 'all' ? `&type=${type}` : ''
 			}`
 		)
@@ -23,7 +23,7 @@ class Main extends React.Component {
 	};
 
 	componentDidMount() {
-		fetch(`http://www.omdbapi.com/?apikey=4abf06ce&s=matrix`)
+		fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
 			.then(response => response.json())
 			.then(data => this.setState({ movies: data.Search, loading: false }));
 	}
