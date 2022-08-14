0121713d5e7f60203d9296ee98b8c39a3e44d229
Vyshniakov Ihor <ihor.vyshniakov@qlicks.nl>
2022-08-14 17:06:20 +0300

    added error handler for API request, and chaged http to https while requesting

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -14,18 +14,26 @@ class Main extends React.Component {
 	searchMovies = (search, type = 'all') => {
 		this.setState({ loading: true });
 		fetch(
-			`http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}${
+			`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}${
 				type !== 'all' ? `&type=${type}` : ''
 			}`
 		)
 			.then(response => response.json())
-			.then(data => this.setState({ movies: data.Search, loading: false }));
+			.then(data => this.setState({ movies: data.Search, loading: false }))
+			.catch(err => {
+				console.error(err);
+				this.setState({ loading: false });
+			});
 	};
 
 	componentDidMount() {
-		fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+		fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
 			.then(response => response.json())
-			.then(data => this.setState({ movies: data.Search, loading: false }));
+			.then(data => this.setState({ movies: data.Search, loading: false }))
+			.catch(err => {
+				console.error(err);
+				this.setState({ loading: false });
+			});
 	}
 
 	render() {
